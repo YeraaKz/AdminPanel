ba8bdc6870c14152b7a19f49fd0a256bc10fd712
Yernar <29525@iitu.edu.kz>
2024-03-27 19:24:17 +0500

    AdminTableSeeder call uncommented in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-//        $this->call(AdminsTableSeeder::class);
+        $this->call(AdminsTableSeeder::class); // email: Admin1@example.com, pass: 123
         $this->call(PostsTableSeeder::class);
     }
 }
